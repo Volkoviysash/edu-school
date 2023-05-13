3f0e328e0f18959549c7940b759fe506a1979fa0
volkovich <volkoviysash@gmail.com>
2023-05-13 03:54:23 +0300

    feat: Add about page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/common/heading/Header.jsx";
+import About from "./components/about/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </Router>
     </>
