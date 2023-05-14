41e8d16c64a5cf5f936f49556f6773da19e34f4e
volkovich <volkoviysash@gmail.com>
2023-05-14 18:17:28 +0300

    fix: Fix bug with hovering team member.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import Header from "./components/common/heading/Header.jsx";
 import About from "./components/about/About";
 import CourseHome from "./components/allcourses/CourseHome";
+import Team from "./components/team/Team";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/courses' element={<CourseHome />} />
+          <Route path='/team' element={<Team />} />
         </Routes>
       </Router>
     </>
