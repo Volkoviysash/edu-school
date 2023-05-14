adba213255818507b4f826bfffdeb70ce926197a
volkovich <volkoviysash@gmail.com>
2023-05-14 20:27:38 +0300

    feat: Add testimonal block.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/Hero";
 import AboutCard from "../about/AboutCard";
 import HAbout from "./hero/HAbout";
+import Test from "./testimonal/test";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero />
       <AboutCard />
       <HAbout />
+      <Test />
     </>
   );
 }
